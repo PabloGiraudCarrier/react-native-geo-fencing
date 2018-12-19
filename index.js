0a8157aa9a9e0c4cf800ecee4177516d1dafcfc0
PabloGiraudCarrier <35684551+PabloGiraudCarrier@users.noreply.github.com>
2018-12-19 18:36:19 +0100

    Update index.js
    change reject() by resolve(false)

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ module.exports = {
           if (isWithinCoverage) {
             resolve(true);
           } else {
-            reject();
+            resolve(false);
           }
         }
       )
